d8b5958dc039a65418034198ec36ece3e3479a51
chucai <hexudong08@gmail.com>
2015-11-24 15:52:22 +0800

    0, 多线程支持发送文件

--- lib/sprockets_cdn/assets_upload.rb
+++ lib/sprockets_cdn/assets_upload.rb
@@ -6,15 +6,34 @@ module SprocketsCDN
       def setup assets, dir, logger
         return unless SprocketsCDN::Configuration.checked?
         require "sprockets_cdn/providers/#{SprocketsCDN::Configuration.config.provider}"
-        kclass = SprocketsCDN::Providers.const_get SprocketsCDN::Configuration.config.provider.classify
-        client = kclass.new SprocketsCDN::Configuration.config
+        kclass      = SprocketsCDN::Providers.const_get SprocketsCDN::Configuration.config.provider.classify
+        client      = kclass.new SprocketsCDN::Configuration.config
         remote_data = {}
-        assets.each do |key, asset|
-          target = File.join(dir, asset)
-          remote_target = client.uploading target, dir: dir
-          logger.info "Uploaded to #{remote_target}"
-          remote_data[key] = remote_target
+
+        lock      = Mutex.new
+
+        keys          = assets.keys
+        assets_count  = keys.size
+        threads       = []
+        threads_count = 20
+        number        = assets_count / threads_count
+
+        keys.in_groups_of(number, false).each do |group|
+          threads << Thread.new do 
+            group.each do |key|
+              asset            = assets[key]
+              target           = File.join(dir, asset)
+              remote_target    = client.uploading target, dir: dir
+              logger.info "Uploaded to #{remote_target}"
+              lock.synchronize do
+                remote_data[key] = remote_target
+              end
+            end
+          end
         end
+
+        threads.each(&:join)
+
         remote_data
       end
     end
